4b1eab43069e309b5ceeb134806142c29793a199
adityasingh1080z@gmail.com <adityasingh1080z@gmail.com>
2025-05-14 08:56:51 +0530

    ✅ Added Todo List Feature  After Signin

--- index.js
+++ index.js
@@ -90,23 +90,52 @@ app.get("/me", auth, function (req, res) {
   });
 });
 
-// app.post("/todo", auth, function (req, res) {
-//   const title = req.body.title;
-//   const description = req.body.description;
+app.post("/todo", auth, function (req, res) {
+  const title = req.body.title;
+  const description = req.body.description;
 
-//   const newTodo = {
-//     id: Date.now(),
-//     title: title,
-//     description: description,
-//     completed: false,
-//   };
+  const newTodo = {
+    id: Date.now(),
+    title: title,
+    description: description,
+    completed: false,
+    username: req.username,
+  };
 
-//   todos.push(newTodo);
+  todos.push(newTodo);
 
-//   res.json({
-//     message: "Todos Added !!!! ✅",
-//   });
+  res.json({
+    message: "Todos Added !!!! ✅",
+  });
+});
 
+app.get("/todos", auth, function (req, res) {
+  const userTodos = todos.filter((t) => t.username === req.username);
+  res.json(userTodos);
+});
 
-// });
+app.put("/todo/:id", auth, function (req, res) {
+  const id = parseInt(req.params.id);
+  const todo = todos.find((t) => t.id === id && t.username === req.username);
+
+  if (todo) {
+    todo.completed = true;
+    res.json({
+      message: "Todo Marked as Done ✅",
+    });
+  } else {
+    res.status(404).json({ message: "Todo not found ❌" });
+  }
+});
+
+app.delete("/delete/:id", auth, function (req, res) {
+  const id = parseInt(req.params.id);
+  const initialLength = todos.length;
+  todos = todos.filter((t) => !(t.id === id && t.username === req.username));
+  if (todos.length < initialLength) {
+    res.json({ message: "Todo deleted ✅" });
+  } else {
+    res.status(404).json({ message: "Todo not found ❌" });
+  }
+});
 app.listen(3000);
